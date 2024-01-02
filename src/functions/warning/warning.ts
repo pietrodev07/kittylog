0b5eddce99808f63772f9069197231fc22eb8922
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-02 12:57:47 +0100

    feat: added `warning.ts`

---
+++ src/functions/warning/warning.ts
@@ -0,0 +1,9 @@
+import { COLORS } from "../../constants";
+import { colorizeText } from "../../utils/colorizeText";
+
+export const warning = (message: string): void => {
+	const label = colorizeText("[WARNING]", COLORS.yellow);
+	const messageColorized = colorizeText(message, COLORS.yellow);
+
+	console.warn(`${label} ${messageColorized}`);
+};
