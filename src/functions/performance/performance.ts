a9f62d316d6ba96405439f91e8d5d34203594429
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-02 13:21:36 +0100

    feat: added `performance.ts`

---
+++ src/functions/performance/performance.ts
@@ -0,0 +1,9 @@
+import { COLORS } from "../../constants";
+import { colorizeText } from "../../utils/colorizeText";
+
+export const performance = (action: string, timeTaken: number): void => {
+	const formattedTime = timeTaken < 1000 ? `${timeTaken}ms` : `${(timeTaken / 1000).toFixed(2)}s`;
+	const label = colorizeText("[PERFORMANCE]", COLORS.cyan);
+
+	console.log(`${label} ${action} - Time taken: ${formattedTime}`);
+};
