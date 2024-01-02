e1cfc087627fc896ce9ff5d1ce8236766ab62273
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-02 12:57:18 +0100

    feat: added `success.ts`

---
+++ src/functions/success/success.ts
@@ -0,0 +1,9 @@
+import { COLORS } from "../../constants";
+import { colorizeText } from "../../utils/colorizeText";
+
+export const success = (message: string): void => {
+	const label = colorizeText("[SUCCESS]", COLORS.green);
+	const messageColorized = colorizeText(message, COLORS.green);
+
+	console.log(`${label} ${messageColorized}`);
+};
