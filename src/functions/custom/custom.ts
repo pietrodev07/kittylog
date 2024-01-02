ad4516ad2653068c637019ef0e374c19b40d1e3e
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-02 13:22:42 +0100

    feat: added `custom.ts`

---
+++ src/functions/custom/custom.ts
@@ -0,0 +1,10 @@
+import { COLORS } from "../../constants";
+import { Color } from "../../types";
+import { colorizeText } from "../../utils/colorizeText";
+
+export const custom = (color: Color, label: string, message: string): void => {
+	const labelMessage = colorizeText(`[${label}]`, COLORS[color]);
+	const messageColorized = colorizeText(message, COLORS[color]);
+
+	console.log(`${labelMessage} ${messageColorized}`);
+};
