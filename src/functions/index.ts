ba52061106fb6ae93e7ed9d3d53ffa417c16473f
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-02 13:29:18 +0100

    chore: exported all functions in the barrel file

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,13 @@
+export { input } from "./input/input";
+export { debug } from "./debug/debug";
 export { error } from "./error/error";
+export { httpResponse } from "./httpResponse/httpResponse";
+export { info } from "./info/info";
+export { performance } from "./performance/performance";
+export { success } from "./success/success";
+export { table } from "./table/table";
+export { tableCSV } from "./tableCSV/tableCSV";
+export { tableJSON } from "./tableJSON/tableJSON";
+export { warning } from "./warning/warning";
+export { blank } from "./blank/blank";
+export { custom } from "./custom/custom";
