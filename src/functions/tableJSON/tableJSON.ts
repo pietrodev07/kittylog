03a7cbe7ced69dc05fc9a32dd99a19d8dd2cd8a1
pietrodev07 <pietro.dev.07@gmail.com>
2024-01-08 15:54:06 +0100

    fix: removed the file reading in `tableJSON.ts`

--- src/functions/tableJSON/tableJSON.ts
+++ src/functions/tableJSON/tableJSON.ts
@@ -1,19 +1,10 @@
-import { readFileSync } from "fs";
-import path from "path";
-import { error } from "../error/error";
 import { info } from "../info/info";
 
-export const tableJSON = (jsonFilePath: string): void => {
-	try {
-		const absolutePath = path.resolve(jsonFilePath);
-		const jsonContent = readFileSync(absolutePath, "utf-8");
-		const jsonData = JSON.parse(jsonContent);
+export const tableJSON = (fileName: string, fileContent: string): void => {
+	const jsonData = JSON.parse(fileContent);
+	
+	const fileNameLabel = `File: ${fileName}`;
+	info(fileNameLabel);
 
-		const fileNameLabel = `File: ${jsonFilePath}`;
-		info(fileNameLabel);
-
-		console.table(jsonData);
-	} catch (err) {
-		error(`An error occurred while reading the JSON file: ${err}`);
-	}
+	console.table(jsonData);
 };
