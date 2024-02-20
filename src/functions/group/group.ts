35788ddc2150823b6cada65c0f0a5602e48eab4a
olga_demina <olga.demina1410@gmail.com>
2024-02-20 17:03:20 +0100

    refactor: new colors handling functionality and devided collapsed function

--- src/functions/group/group.ts
+++ src/functions/group/group.ts
@@ -1,17 +1,21 @@
 import { COLORS } from "../../constants";
-import { colorizeText } from "../../utils/colorizeText";
+import { colorsProvider } from "../../global";
+import { Color } from "../../types";
 
-export const group = (action: string, isCollapsed = false) => {
-  const label = colorizeText(`[${action}]`, COLORS.magenta);
+export const group = (title: string, color?: Color) => {
+  const { colorizeText, buildCompleteMessage } = colorsProvider;
+  const selectedColor = color in COLORS ? COLORS[color] : COLORS.magenta;
+  const label = colorizeText(`[${title}]`, selectedColor);
+  console.group(...buildCompleteMessage([label]));
+};
 
-  if(isCollapsed) {
-    console.groupCollapsed(label);
-  } else {
-    console.group(label);
-  }
-}
+export const groupCollapsed = (title: string, color?: Color) => {
+  const { colorizeText, buildCompleteMessage } = colorsProvider;
+  const selectedColor = color in COLORS ? COLORS[color] : COLORS.magenta;
+  const label = colorizeText(`[${title}]`, selectedColor);
+  console.groupCollapsed(...buildCompleteMessage([label]));
+};
 
 export const groupEnd = () => {
   console.groupEnd();
-}
-
+};
